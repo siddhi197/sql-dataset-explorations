779db23c9b74d0a0bd0387d34970f190112fd8e2
Siddhi Kholkar <51523980+siddhi197@users.noreply.github.com>
2023-03-21 12:36:37 -0400

    Update Data Cleaning Queries.sql

--- Data Cleaning Queries.sql
+++ Data Cleaning Queries.sql
@@ -153,6 +153,85 @@ From
 
 -- Change Y and N to Yes and No in "Sold as Vacant" field
 
+Select 
+     Distinct(SoldAsVacant), 
+     Count(SoldAsVacant)
+From PortfolioProject.NashvilleHousing
+Group by SoldAsVacant
+order by 2
+
+
+
+
+Select 
+     SoldAsVacant,
+     CASE When SoldAsVacant = 'Y' THEN 'Yes'
+	     When SoldAsVacant = 'N' THEN 'No'
+	   ELSE SoldAsVacant
+	   END
+From PortfolioProject.dbo.NashvilleHousing
+
+
+Update NashvilleHousing
+SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
+	   When SoldAsVacant = 'N' THEN 'No'
+	   ELSE SoldAsVacant
+	   END
+
+
+
+
+
+
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Remove Duplicates
+
+WITH RowNumCTE AS(
+Select 
+     *,
+	ROW_NUMBER() OVER (
+	PARTITION BY ParcelID,
+				 PropertyAddress,
+				 SalePrice,
+				 SaleDate,
+				 LegalReference
+				 ORDER BY
+					UniqueID
+					) row_num
+
+From PortfolioProject.NashvilleHousing
+)
+DELETE *
+From RowNumCTE
+Where row_num > 1
+Order by PropertyAddress
+
+
+
+Select *
+From PortfolioProject.dbo.NashvilleHousing
+
+
+
+
+---------------------------------------------------------------------------------------------------------
+
+-- Delete Unused Columns
+
+
+
+Select *
+From PortfolioProject.NashvilleHousing
+
+
+ALTER TABLE PortfolioProject.NashvilleHousing
+DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress, SaleDate
+
+
+
 
 
 
